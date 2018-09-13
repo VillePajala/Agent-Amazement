c61e28d19f54367c8896e8ba649d0435161642f8
VillePajala <valoraami@gmail.com>
2018-09-13 15:33:58 +0300

    Added enemies to gameplay
    - The game now has very dangerous enemies you can shoot at

---
+++ Agent Amazement/Assets/Scripts/TrumpController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrumpController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Equals("BulletToRight"))
+        {
+            Destroy(gameObject);
+   
+        }
+
+        if (collision.name.Equals("BulletToLeft"))
+        {
+            Destroy(gameObject);
+
+        }
+
+        if (collision.name.Equals("BulletToDown"))
+        {
+            Destroy(gameObject);
+
+        }
+
+        if (collision.name.Equals("BulletToUp"))
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
